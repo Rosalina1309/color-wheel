4b2fed62a703525d2834b12808b458fdc05f61da
Rosalina <hongnhungpham130995@gmail.com>
2025-05-06 12:21:48 +0200

    feat: initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { ColorWheelComponent } from "../components/color-wheel/color-wheel.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, ColorWheelComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
---
+++ src/components/color-wheel/color-wheel.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-color-wheel',
+  standalone: true,
+  imports: [],
+  templateUrl: './color-wheel.component.html',
+  styleUrl: './color-wheel.component.scss'
+})
+export class ColorWheelComponent {
+
+}
